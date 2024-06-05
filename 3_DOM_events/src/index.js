e47b9ba3f6dc3e25bfc522d7bed864cb640ec802
Justin Steffen <jcsteffen03@gmail.com>
2024-06-05 11:46:32 -0600

    Completed Lecture

--- 3_DOM_events/src/index.js
+++ 3_DOM_events/src/index.js
@@ -1,8 +1,14 @@
 // Lets start by reviewing object manipulation
 // Objects are a collection of data in a key value pair format
-const obj = {}
+const obj = {
+  keyoverhere: "valuepairoverhere"
+}
 const userInfo = {
-  firstName: "Avi",
+  firstName: {
+    n1: "Avi",
+    n2: "Av",
+    n3: "A"
+  },
   lastName: "Flombaum",
   companyName: "Flatbook Labs",
   jobTitle: "Developer Apprentice",
@@ -24,7 +30,14 @@ const userInfo = {
 
 // We can use . or brackets in order to get different keys from the data
 //// We can iterate through this using a for loop!
+/*
+const key = "firstname"
+console.log(userInfo.jobTitle)
 
+for(let key in userInfo){
+  console.log(userInfo[key])
+}
+*/
 // We can nest within the object as we've seen before!
 
 // Now let us start grabbing specific data from that
@@ -37,18 +50,100 @@ const userInfo = {
 
 // A common event listener you will see, why is that? What does it do?
 // document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => { // also can use defer befroe src script
+  const header = document.querySelector("h1")
+    header.addEventListener("click",(event)=>{
+      console.log(event)
+      if (header.textContent === bookStore.name){
+         header.textContent = bookStore.address
+       } else {
+         header.textContent = bookStore.name
+       }
+      })
 
 // Renders Header
-function renderHeader(){
+  function renderHeader(){
     document.querySelector('h1').textContent = bookStore.name;
-}
+  }
 // Renders Footer
-function renderFooter(){
+  function renderFooter(){
     const footerDivs = document.querySelectorAll('footer div');
     footerDivs[0].textContent = bookStore.name;
     footerDivs[1].textContent = bookStore.address;
     footerDivs[2].textContent = bookStore.hours;
-}
+  }
+
+
+  renderFooter()
+  renderHeader()
+
+
+
+  bookStore.inventory.forEach(displayBook)
+      /*
+      div.addEventListener("mouseenter",()=>{
+        div.append(image)
+      })
+
+      div.addEventListener("mouseleave",()=>{
+        image.remove()
+      })
+      */
+
+    
+
+    const bookForm = document.querySelector("#book-form")
+    bookForm.addEventListener("submit",(event)=>{
+      event.preventDefault()
+
+      const newBook = {
+        title:event.target.title.value,
+        author:event.target.author.value,
+        price:event.target.price.value,
+        imageUrl:event.target.imageUrl.value,
+      }
+      displayBook(newBook)
+    })
+
+})
+
+
+function displayBook(book){
+    const book_list = document.querySelector("#book-list")
+    const div = document.createElement("div")
+    const title = document.createElement("h3")
+    const auth = document.createElement("p")
+    const price = document.createElement("p")
+    const image = document.createElement("img")
+    const button = document.createElement("button")
+
+    title.textContent = book.title
+    auth.textContent = book.author
+    price.textContent = book.price
+    image.src = book.imageUrl
+    button.textContent = "Show More"
+    
+    div.append(title,auth,price,button)
+    book_list.append(div)
+
+    let displayImage = false
+
+      button.addEventListener("click",()=>{
+        if(displayImage){
+          image.remove()
+          displayImage = false
+          button.textContent = "Show More"
+          } 
+        else {
+            div.append(image)
+            displayImage = true
+            button.textContent = "Show Less"
+        }
+
+      })
+    }
+    
+
 
 // Let us create a function that rerenders each of the books
 
--- 3_DOM_events/src/review.js
+++ 3_DOM_events/src/review.js
@@ -1,3 +1,46 @@
 // Array manipulation
+arr = [1,2,3,4]
+fruits = ["apple","orange"]
+/*
+arr.push(5)
+arr.unshift(0)
+arr.pop()
+arr.shift()
+arrCopy = [...]
+arr.slice(-2)
+arr.splice(1,2)
+
+// Map Function
+
+const mapReturn = arr.map((item)=>{
+    console.log(item)
+    return item * 10
+}) 
+
+console.log(mapReturn)
+
+// forEach Function
+
+arr.forEach((item) => {
+    console.log(item)
+});
 
 // Query Selectors
+
+console.log(document.querySelector("div main"))
+const vari = document.querySelector("#id")
+document.querySelector(".class")
+*/
+
+/*
+
+const header = document.querySelector("h1")
+header.textContent = "This is a Header"
+header.id = "Unique Id"
+
+const headerDiv = document.querySelector("div header div")
+
+const img = document.createElement("img")
+headerDiv.append(img)
+
+*/
